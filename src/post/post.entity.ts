0e6893faa5e8e938fadddf6c4059d09b931c910b
joaoh <joao_camargo10@hotmail.com>
2020-03-12 21:40:13 -0300

    Crud validations

--- src/post/post.entity.ts
+++ src/post/post.entity.ts
@@ -3,17 +3,28 @@ import { UserEntity } from 'src/user/user.entity'
 import { GenericEntity } from 'src/generic/generic.entity'
 import { CommentEntity } from 'src/comment/comment.entity'
 import { LikeEntity } from 'src/like/like.entity'
+import { IsOptional, IsDefined, IsString, IsNumber, IsEmpty } from 'class-validator'
+import { CrudValidationGroups } from '@nestjsx/crud'
+
+const { CREATE, UPDATE } = CrudValidationGroups
 
 @Entity({ name: 'posts' })
 export class PostEntity extends GenericEntity {
 
     @PrimaryGeneratedColumn()
+    @IsOptional({ always: true })
     id: number
 
     @Column({ length: 50 })
+    @IsDefined({ groups: [CREATE] })
+    @IsOptional({ groups: [UPDATE] })
+    @IsString({ always: true })
     title: string
 
     @Column('text')
+    @IsDefined({ groups: [CREATE] })
+    @IsOptional({ groups: [UPDATE] })
+    @IsString({ always: true })
     body: string
 
     @ManyToOne(() => UserEntity, (user: UserEntity) => user.posts, { onUpdate: 'CASCADE', onDelete: 'CASCADE' })
@@ -27,6 +38,7 @@ export class PostEntity extends GenericEntity {
     likes: LikeEntity[]
 
     @Column({ default: 0, type: 'int' })
+    @IsEmpty({ always: true, message: 'hey.. I know' })
     comments_num: number
 
     @Column({ type: 'number' })
